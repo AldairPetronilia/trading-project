48689ee560212bbc5dafed899476a06421ad9394
Aldair Petronilia (Mac) <aldairpetronilia@gmail.com>
2025-06-28 21:01:35 +0100

    Initial Commit
    * Set up the folder for the data service
    * Set up git commit hooks

---
+++ energy_data_service/main.py
@@ -0,0 +1,14 @@
+"""Energy Data Service main module."""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def main() -> None:
+    """Main function for the energy data service."""
+    log.info("Hello from energy_data_service!")
+
+
+if __name__ == "__main__":
+    main()
