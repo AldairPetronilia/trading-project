996249b92b56d47b3bea682ddd5fe8a21cbbe301
Aldair Petronilia (Mac) <aldairpetronilia@gmail.com>
2025-07-09 20:45:53 +0100

    ENTSOE Python Client - Add the xml adapters

--- entsoe_client/src/entsoe_client/model/common/area_code.py
+++ entsoe_client/src/entsoe_client/model/common/area_code.py
@@ -1,7 +1,9 @@
 import re
 from enum import Enum
-from typing import Self
+from typing import Any, Self
 
+from pydantic import GetCoreSchemaHandler
+from pydantic_core import CoreSchema, core_schema
 from src.entsoe_client.exceptions.unknown_area_code_error import UnknownAreaCodeError
 from src.entsoe_client.model.common.area_type import AreaType
 
@@ -466,3 +468,27 @@ class AreaCode(Enum):
 
         # 4. If no code is found in any of the sources, return None
         return None
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        source_type: Any,
+        handler: GetCoreSchemaHandler,
+    ) -> CoreSchema:
+        from_code_validator = core_schema.no_info_plain_validator_function(
+            cls.from_code,
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_code_validator,
+            # For creating models from Python, we can be more flexible:
+            # allow passing an existing AreaCode instance OR a string to be parsed.
+            python_schema=core_schema.union_schema(
+                [core_schema.is_instance_schema(cls), from_code_validator],
+            ),
+            # For serialization (e.g., model_dump), define the 'marshal' logic.
+            # We take an AreaCode instance and return its .code attribute.
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda v: v.code,
+            ),
+        )
