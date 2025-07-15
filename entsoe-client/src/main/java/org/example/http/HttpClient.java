94718110358b907b7b33fe8b2cb0e57526719391
Aldair Petronilia (Mac) <aldairpetronilia@gmail.com>
2025-07-15 21:22:51 +0100

    Trading-Project: Remove unnecessary classes

--- entsoe-client/src/main/java/org/example/http/HttpClient.java
+++
@@ -1,9 +0,0 @@
-package org.example.http;
-
-import java.util.Map;
-
-public interface HttpClient {
-  String get(String url, Map<String, String> parameters) throws HttpClientException;
-
-  void close();
-}
